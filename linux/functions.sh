4ce2cb1ec0f5754ce2c80f0bf04c0774a1aa1e22
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-06 20:20:33 +0000

    Fix in-place rename

--- linux/functions.sh
+++ linux/functions.sh
@@ -11,3 +11,15 @@ copy() {
         xclip -sel c < $@
     fi
 }
+
+# https://gist.github.com/premek/6e70446cfc913d3c929d7cdbfe896fef
+mv() {
+  if [ "$#" -ne 1 ] || [ ! -e "$1" ]; then
+    command mv "$@"
+    return
+  fi
+
+  read -e -p"🔀 " -i $1 newfilename
+  command mv -v -- "$1" "$newfilename"
+}
+
--- macos/functions.sh
+++ macos/functions.sh
@@ -3,6 +3,18 @@ copy() {
     cat $1 | pbcopy
 }
 
+# https://gist.github.com/premek/6e70446cfc913d3c929d7cdbfe896fef
+mv() {
+  if [ "$#" -ne 1 ] || [ ! -e "$1" ]; then
+    command mv "$@"
+    return
+  fi
+
+  newfilename=$1
+  vared -p "🔀 " newfilename  # adapted to zsh via https://stackoverflow.com/a/53543070
+  command mv -v -- "$1" "$newfilename"
+}
+
 # init a R project based on the PWD name
 rinit() {
     DIR=${PWD##*/}
