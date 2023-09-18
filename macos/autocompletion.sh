661ab020127506d529e8d8ca34ba4908e3b8c3a6
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-09-19 00:51:25 +0100

    Add completions for justfiles

--- macos/autocompletion.sh
+++ macos/autocompletion.sh
@@ -7,6 +7,11 @@ list_python_versions() {
     COMPREPLY=$(lspy)
 }
 
+list_just_targets() {
+    COMPREPLY=$(just -l | tail -n +1 | perl -nle 'print $1 if /^\s+(\S+)/')
+}
+
 complete -F list_virtualenvs workon
 complete -F list_virtualenvs rmvirtualenv
 complete -F list_python_versions mkvirtualenv
+complete -F list_just_targets just
