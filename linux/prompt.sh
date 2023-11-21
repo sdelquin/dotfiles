8a5251ace531677c031815a285f6418d3cd5cd9e
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-21 20:59:50 +0000

    Fix prompt for python virtualenv

--- linux/prompt.sh
+++ linux/prompt.sh
@@ -1,4 +1,5 @@
 RUNNING_SYMBOL='✨'
+VIRTUALENV_SYMBOL='»'
 
 parse_git_branch() {
     branch=$(git branch 2> /dev/null | sed -e 's/* \(.*\)/\1/')
@@ -6,9 +7,15 @@ parse_git_branch() {
 }
 
 parse_virtualenv() {
-    if [[ ! -z "${VIRTUAL_ENV}" ]]; then
-        virtualenv=$(basename $VIRTUAL_ENV)
-        echo "[⧩ $virtualenv] "
+    if [[ "${VIRTUAL_ENV}" ]]; then
+        venv_cfg=$VIRTUAL_ENV/pyvenv.cfg
+        if [[ -f "$venv_cfg" ]]; then
+            virtualenv=$(perl -ne 'print $1 if /^prompt *= *\x27(.*)\x27$/' $venv_cfg)
+        fi
+        if [[ -z $virtualenv ]]; then
+            virtualenv=$(basename $VIRTUAL_ENV)
+        fi
+        echo "[$VIRTUALENV_SYMBOL $virtualenv] "
     fi
 }
 
@@ -18,4 +25,4 @@ check_git_changes() {
     fi
 }
 
-export PS1="$RUNNING_SYMBOL\[\033[01;35m\]\$(parse_virtualenv)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[01;33m\]\$(check_git_changes)\[\e[00m\]$ "
+export PS1="$RUNNING_SYMBOL\[\033[01;32m\]\$(parse_virtualenv)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;35m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[01;33m\]\$(check_git_changes)\[\e[00m\]$ "
