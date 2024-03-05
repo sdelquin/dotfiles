5ddba3fa0883dca8a102b576193c13dc773372e6
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-03-05 09:52:46 +0000

    Fix python issues

--- common/aliases.sh
+++ common/aliases.sh
@@ -1,14 +1,14 @@
-alias clean_pyc="find . -name *.pyc -exec rm {} \;"
-alias serve="python -m http.server"
 alias tree="tree -C"
 alias ll='lsd -F -l --date relative'
 alias lla='lsd -F -la --date relative'
 alias bat='bat -p'
 alias g='rg'
 alias f='fd'
-alias c='cd ~/code'
-alias v='source .venv/bin/activate'
 alias j='just'
+# Python
+alias v='source .venv/bin/activate'
+alias p='clear; ipython -i $DOTFILES/common/ipython.py'
+alias serve="python -m http.server"
+alias clean_pyc="find . -name *.pyc -exec rm {} \;"
 alias d='deactivate'
-alias p='ipython'
 alias upip='pip install -U pip'
--- common/exports.sh
+++ common/exports.sh
@@ -1,2 +1,1 @@
-export PYTHONSTARTUP=$DOTFILES/common/ipython.py
 export PATH=$PATH:$HOME/.local/bin
