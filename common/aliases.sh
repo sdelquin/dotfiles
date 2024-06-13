f628d9d0cc5ceb32f8056aa1babf8698612ec2bb
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-06-13 17:57:22 +0100

    Add docker aliases

--- common/aliases.sh
+++ common/aliases.sh
@@ -12,3 +12,8 @@ alias serve="python -m http.server"
 alias clean_pyc="find . -name *.pyc -exec rm {} \;"
 alias d='deactivate'
 alias upip='pip install -U pip'
+# Docker
+alias rmdockerexitedcontainers='docker ps -a | perl -nale '\''print $F[0] if /Exited/'\'' | xargs docker rm'
+alias rmdockeranonvolumes='docker volume ls | perl -nale '\''print $F[1] if length($F[1]) == 64'\'' | xargs docker volume rm'
+alias rmdockerorphanimages='docker images | perl -nale '\''print $F[2] if $F[0] =~ /<none>/'\'' | xargs docker rmi'
+alias rmdocker='rmdockerexitedcontainers && rmdockeranonvolumes && rmdockerorphanimages'
