47eb144e1b0293fa8725b1052ac6a94912f4f8e9
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-07-24 19:18:37 +0100

    Fix pyenv path issues

--- common/pyenv-init.sh
+++ common/pyenv-init.sh
@@ -1,4 +1,4 @@
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+#eval "$(pyenv virtualenv-init -)"
