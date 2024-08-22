fbd634a4e0cf6fbc4ef6a0727500f782daabf96c
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-22 13:28:23 +0100

    Get rid of unused addreq function

--- common/functions.sh
+++ common/functions.sh
@@ -1,14 +1,3 @@
-function addreq () {
-    if grep -iq $1 requirements.txt; then
-        echo \"$1\" already exists in requirements.txt
-    else
-        pip install $1
-        pin=$(pip freeze | grep -i $1)
-        echo Adding \"$pin\" to requirements.txt
-        echo $pin >> requirements.txt
-    fi
-}
-
 function cd() {
     # https://stackoverflow.com/a/50830617
 
