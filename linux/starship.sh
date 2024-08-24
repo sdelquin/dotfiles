02c7fa2a8ca8b97d24267cf213efbbbde0ec64d8
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-24 10:03:59 +0100

    Fix minor prompt issues for Linux

--- linux/starship.sh
+++ linux/starship.sh
@@ -3,9 +3,9 @@ show_newline() {
     # https://bit.ly/4dCTtVC
     if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
         NEW_LINE_BEFORE_PROMPT=1
-    elif ["$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+    elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
         echo ""
     fi
 }
 PROMPT_COMMAND="show_newline"
-eval "$(starship init zsh)"
+eval "$(starship init bash)"
