592ea8b12c12cf9195e723f5c7cf7f31136f33a8
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-09-05 18:38:38 +0100

    Set ll as default enter command

--- macos/exports.sh
+++ macos/exports.sh
@@ -4,7 +4,7 @@ export DOCKER_CLI_HINTS=false
 # brew install bat
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/magic-enter
-MAGIC_ENTER_GIT_COMMAND='git status'
+MAGIC_ENTER_GIT_COMMAND='ll'
 MAGIC_ENTER_OTHER_COMMAND='ll'
 # PATH
 export PATH=$PATH:$HOMEBREW_PREFIX/opt/python@3.12/libexec/bin:$HOME/bin
