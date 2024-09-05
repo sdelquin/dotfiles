98a9c1298e3aa9462bf735e5442df889720bafef
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-09-05 18:37:42 +0100

    Fix venv autoactivate

--- common/functions.sh
+++ common/functions.sh
@@ -1,6 +1,6 @@
-function cd() {
+cd() {
     # https://stackoverflow.com/a/50830617
-
+    #
     # check if zxoide is installed
     if ! [ -x "$(command -v __zoxide_z)" ]; then
         __zoxide_z "$@"
@@ -8,10 +8,22 @@ function cd() {
         builtin cd "$@"
     fi
 
+    venv_autoactivate
+}
+
+venv_autoactivate() {
+    # https://github.com/pypa/virtualenv/issues/554#issuecomment-35713201
+    # https://stackoverflow.com/a/50830617
     if [[ -z "$VIRTUAL_ENV" ]] ; then
-        ## If env folder is found then activate the vitualenv
-        if [[ -d ./.venv ]] ; then
-            source ./.venv/bin/activate
+        dir=`pwd`
+        while [[ ! -f $dir/.venv/bin/activate && -n $dir ]]; do
+            dir=${dir%/*}
+        done
+        if [[ -f $dir/.venv/bin/activate ]]; then
+            #echo "Activating virtualenv from $dir/venv"
+            source $dir/.venv/bin/activate
+        else
+            #echo 'Virtualenv not found'
         fi
     else
         ## check the current folder belong to earlier VIRTUAL_ENV folder
@@ -20,6 +32,7 @@ function cd() {
         parentdir="$(dirname "$VIRTUAL_ENV")"
         if [[ "$PWD"/ != "$parentdir"/* ]] ; then
             deactivate
+            venv_autoactivate  # maybe we jumped into a new virtualenv!
         fi
     fi
 }
