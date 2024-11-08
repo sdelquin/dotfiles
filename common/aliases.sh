04204b48faae031f228595cfc73daafe2a55b6c7
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-11-08 12:26:16 +0000

    Add alias to disable auto activation of virtualenvs

--- common/aliases.sh
+++ common/aliases.sh
@@ -23,6 +23,7 @@ alias serve="python -m http.server"
 alias clean_pyc="find . -name '*.pyc' -exec rm {} \;"
 alias fpp='pip freeze | grep -i'
 alias cdsitepackages='cd $(find . -name site-packages -d)'
+alias disable-auto-venv='export DISABLE_AUTO_VENV=1'
 # Docker
 alias rmdockerexitedcontainers='docker ps -a | perl -nale '\''print $F[0] if /Exited/'\'' | xargs docker rm'
 alias rmdockeranonvolumes='docker volume ls | perl -nale '\''print $F[1] if length($F[1]) == 64'\'' | xargs docker volume rm'
