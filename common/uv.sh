3a114d5da6057a4537f29d9009cd688349ad67c5
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-03-11 09:39:03 +0000

    Add new functions to manage python package installs

--- common/uv.sh
+++ common/uv.sh
@@ -6,3 +6,32 @@ mkvirtualenv() {
     fi
     [[ $? -eq 0 ]] && source .venv/bin/activate
 }
+
+pi() {
+    REQFILE="requirements.txt"
+    [ ! -f $REQFILE ] && touch $REQFILE
+    for package in $@; do
+        uv pip install $package
+        package_qual=`uv pip freeze | grep $package==`
+        package_name=`echo $package_qual | perl -ne 'print $1 if /(.*)==.*/'`
+        package_version=`echo $package_qual | perl -ne 'print $1 if /.*==(.*)/'`
+        if grep -q $package_name $REQFILE; then
+            perl -i -pe "s/^$package_name==.*$/$package_qual/" $REQFILE
+        else
+            echo $package_qual >> $REQFILE
+        fi
+    done
+    sort $REQFILE -o $REQFILE
+    echo Updated $REQFILE
+}
+
+pu() {
+    REQFILE="requirements.txt"
+    for package in $@; do
+        uv pip uninstall $package
+        if [ -f $REQFILE ] && grep -q $package $REQFILE; then
+            perl -i -ne "print unless /^$package==.*$/" $REQFILE
+            echo Updated $REQFILE
+        fi
+    done
+}
