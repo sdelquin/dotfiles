24ee469d2529ec2226890e81fe432edc2993ab25
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-04-07 15:31:06 +0100

    Fix activation of uv/venv

--- common/functions.sh
+++ common/functions.sh
@@ -13,39 +13,41 @@ cd() {
 
 manage_envs() {
     uv_autoactivate
-    if [ -z "${DISABLE_AUTO_VENV}" ] && [ -z "${UV_ENV}" ]; then
-        venv_autoactivate
-    fi
+    venv_autoactivate
 }
 
 venv_autoactivate() {
     # https://github.com/pypa/virtualenv/issues/554#issuecomment-35713201
     # https://stackoverflow.com/a/50830617
-    if [[ -z "$VIRTUAL_ENV" ]] ; then
-        dir=`pwd`
-        while [[ ! -f $dir/.venv/bin/activate && -n $dir ]]; do
-            dir=${dir%/*}
-        done
-        if [[ -f $dir/.venv/bin/activate ]]; then
-            #echo "Activating virtualenv from $dir/venv"
-            source $dir/.venv/bin/activate
-        #else
-            #echo 'Virtualenv not found'
-        fi
-    else
-        ## check the current folder belong to earlier VIRTUAL_ENV folder
-        # if yes then do nothing
-        # else deactivate
-        parentdir="$(dirname "$VIRTUAL_ENV")"
-        if [[ "$PWD"/ != "$parentdir"/* ]] ; then
-            deactivate
-            venv_autoactivate  # maybe we jumped into a new virtualenv!
+    if [ -z $DISABLE_AUTO_VENV ]; then
+        if [ -z $VIRTUAL_ENV ]; then
+            if [ -z $UV_ENV ]; then
+                dir=`pwd`
+                while [[ ! -f $dir/.venv/bin/activate && -n $dir ]]; do
+                    dir=${dir%/*}
+                done
+                if [[ -f $dir/.venv/bin/activate ]]; then
+                    #echo "Activating virtualenv from $dir/venv"
+                    source $dir/.venv/bin/activate
+                #else
+                    #echo 'Virtualenv not found'
+                fi
+            fi
+        else
+            ## check the current folder belong to earlier VIRTUAL_ENV folder
+            # if yes then do nothing
+            # else deactivate
+            parentdir="$(dirname "$VIRTUAL_ENV")"
+            if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+                deactivate
+                venv_autoactivate  # maybe we jumped into a new virtualenv!
+            fi
         fi
     fi
 }
 
 uv_autoactivate() {
-    if [[ -z "$UV_ENV" ]] ; then
+    if [ -z $UV_ENV ] ; then
         dir=`pwd`
         while [[ ! -f $dir/uv.lock && -n $dir ]]; do
             dir=${dir%/*}
