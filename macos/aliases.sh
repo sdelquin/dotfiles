6954ad7d0b4cede0d94a2354622cec4114a0a872
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-07-18 09:54:53 +0100

    Update some aliases for MacOS

--- macos/aliases.sh
+++ macos/aliases.sh
@@ -6,9 +6,6 @@ alias kstats='sudo killall systemstats'
 alias kfinder='sudo killall Finder'
 alias kcode='kill -9 `ps aux | grep -i "code helper" | tr -s " " | grep -v grep | cut -d" " -f2 | xargs`'
 alias o='open .'
-alias sync-alu="scp *.JPG hetzner:/usr/share/fotos/alumnado && mv *.JPG procesadas"
-alias sync-prof="scp *.JPG hetzner:/usr/share/fotos/profesorado && mv *.JPG procesadas"
-alias sync-pas="scp *.JPG hetzner:/usr/share/fotos/pas && mv *.JPG procesadas"
 alias mount-medusa="open 'smb://sdelqui@TFQSC501/Usuarios/sdelqui'"
 alias iface='/sbin/ifconfig | perl -nle '"'"'print $1 if /inet *(\d+\.\d+\.\d+\.\d+)/'"'"' | grep -v ^127'
 alias mserve='serve -b $(iface)'
@@ -18,3 +15,7 @@ alias nf="notes find"
 alias ns="notes search"
 alias no="notes open"
 alias nc="notes cat"  # /usr/local/bin/notes parcheado en L315 (https://n9.cl/89ip3)
+alias marked='open -a "Marked 2"'
+# Cutting Optimization Pro (via Whisky App)
+alias ematerials='vi "/Users/sdelquin/Library/Containers/com.isaacmarovitz.Whisky/Bottles/74DF8116-2E4C-4535-8B11-2E8D09548484/drive_c/users/crossover/Documents/Optimal Programs/Cutting Optimization pro 5/materials.xml"'
+alias estock='vi "/Users/sdelquin/Library/Containers/com.isaacmarovitz.Whisky/Bottles/74DF8116-2E4C-4535-8B11-2E8D09548484/drive_c/users/crossover/Documents/Optimal Programs/Cutting Optimization pro 5/stock2d.xml"'
