82ee63223112b92cb485a48b48091f84f4ea8475
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-10-09 09:43:39 +0100

    Fix ruff configuration

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.config/mypy
+mkdir -p ~/.config/ruff
 mkdir -p ~/.ipython/profile_default
 
 # prompt
@@ -12,7 +13,7 @@ ln -s ~/.dotfiles/common/latexmkrc ~/.latexmkrc
 ln -s ~/.dotfiles/common/gitconfig ~/.gitconfig
 
 # python tools
-ln -s ~/.dotfiles/common/ruff.toml ~/.config/ruff.toml
+ln -s ~/.dotfiles/common/ruff.toml ~/.config/ruff/ruff.toml
 ln -s ~/.dotfiles/common/mypy.ini ~/.config/mypy/config
 ln -s ~/.dotfiles/common/ipython.py ~/.ipython/profile_default/ipython_config.py
 ln -s ~/.dotfiles/common/radian ~/.radian_profile
