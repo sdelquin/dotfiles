e5aadc37da7a7019240e819c49b91b41e6307882
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-11-13 18:22:02 +0000

    Add alias for pytest

--- common/aliases.sh
+++ common/aliases.sh
@@ -24,7 +24,7 @@ alias clean_pyc="find . -name '*.pyc' -exec rm {} \;"
 alias fpp='pip freeze | grep -i'
 alias cdsitepackages='cd $(find . -name site-packages -d)'
 alias disable-auto-venv='export DISABLE_AUTO_VENV=1'
-alias justdjango='curl -fsSL -o justfile https://raw.githubusercontent.com/sdelquin/justfiles/refs/heads/main/justfile.django'
+alias t='pytest'
 # uv
 alias uva='uv_autoactivate'
 # Docker
