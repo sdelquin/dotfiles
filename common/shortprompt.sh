af9050a03f864abce1ffc2f153ea7ce3914a5d5f
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-12-03 20:11:20 +0000

    Fix short prompt to 2 folders

--- common/shortprompt.sh
+++ common/shortprompt.sh
@@ -1,2 +1,2 @@
-cat ~/.dotfiles/common/starship.toml | perl -pe 's/(truncation_length) *= (.*)/$1 = 3/' > /tmp/starship.toml
+cat ~/.dotfiles/common/starship.toml | perl -pe 's/(truncation_length) *= (.*)/$1 = 2/' > /tmp/starship.toml
 export STARSHIP_CONFIG=/tmp/starship.toml
